beb9d2d9e832131e54b1dbf3b101f88cc65303a4
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-03-09 17:01:30 -0300

    Validating check-in distance.

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe("Check-in Use Case", () => {
       title: "JavaScript Gym",
       description: "",
       phone: "",
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-23.1073521),
+      longitude: new Decimal(-47.2186047),
     });
 
     vi.useFakeTimers();
@@ -81,5 +81,25 @@ describe("Check-in Use Case", () => {
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to check in on distant gym", async () => {
+    gymsRepository.items.push({
+      id: "gym-02",
+      title: "JavaScript Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(-23.0795007),
+      longitude: new Decimal(-47.1995609),
+    });
+
+    await expect(() =>
+      sut.execute({
+        gymId: "gym-02",
+        userId: "user-01",
+        userLatitude: -23.1073521,
+        userLongitude: -47.2186047,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
