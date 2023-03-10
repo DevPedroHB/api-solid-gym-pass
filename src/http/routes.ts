ff18b86ce065e164435999933068167db80f6e40
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-03-10 20:52:48 -0300

    Creating test environments.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { authenticate } from "./controllers/authenticate";
 import { profile } from "./controllers/profile";
 import { register } from "./controllers/register";
-import { verifyJWT } from "./middlewares/verify-jwt";
+import { verifyJWT } from "./middlewares/middleware";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
