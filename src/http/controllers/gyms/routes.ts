553a75966d657bd22ba4b2a86d3d98c2991ae259
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2023-03-11 17:21:00 -0300

    Other Academy controllers.

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from "@/http/middlewares/middleware";
 import { FastifyInstance } from "fastify";
+import { create } from "./create";
+import { nearby } from "./nearby";
+import { search } from "./search";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
